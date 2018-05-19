cdbc2765a6de134e80d035ea62fa35a520e54181
Ksevoz3101 <ksevoz3101@mail.ru>
2018-05-20 00:27:51 +0300

    исправление ошибок

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,24 +10,8 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sim1= '*';
-
-            Draw(x1, y1, sim1);
-
-            int x2 = 4;
-            int y2 = 6;
-            char sim2 = '#';
-
-            Draw(x2, y2, sim2);
-            
+            Console.WriteLine("Hello world!");
             Console.ReadLine();
-        }
-        static void Draw(int x, int y, char sim)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.WriteLine(sim);
         }
     }
 }
