d1a95045889119b01d29c6169f3f40b9d59156d9
Ksevoz3101 <ksevoz3101@mail.ru>
2018-05-20 12:54:44 +0300

    разбор переменных их хранения в ячейках и адресов к функциям(кучи)

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,13 +10,51 @@ namespace snake
     {
         static void Main(string[] args)
         {
+            int x = 1;
+            Func1(x);
+            Console.WriteLine("Func1. х = " + x);
+
+            Func2(x);
+            Console.WriteLine("Func2. х = " + x);
+
+            Func3(x);
+            Console.WriteLine("Func3. х = " + x);
+
             Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Move(p1, 10, 10);
+            Console.WriteLine("Координаты точки по х =" + p1.x + " а по у = " + p1.y);
 
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            p1 = p2;
+            p2.x = 7;
+            p2.y = 7;
+            Console.WriteLine("p1.x = " + p1.x + ", p1.y = " + p1.y + "and p2.x = " + p2.x + " , p2.y ="+ p2.y);
 
+            p1 = new Point(1, 3, '*');
+            Update(p1);
+            Console.WriteLine("p1.x = "+ p1.x + ", p1.y = "+ p1.y);
             Console.ReadLine();
+       }
+        
+        public static void Func1(int value)
+        {
+            value = value + 1;
+        }
+        public static void Func2(int value)
+        {
+            value = value + 2;
+        }
+        public static void Func3(int value)
+        {
+            value = value + 3;
+        }
+        public static void Move(Point p, int dx, int dy)
+        {
+            p.x = p.x + dx;
+            p.y = p.y + dy;
+        }
+        public static void Update(Point p)
+        {
 
         }
 
